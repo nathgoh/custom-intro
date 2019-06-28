5f2fbeb9319d74f665630a1b4f8c044b5f13ce81
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-06-28 00:22:06 -0700

    custom introduction

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, TouchableHighlight, TouchableOpacity, Text, View } from 'react-native';
+import { Alert, StyleSheet, Button, TextInput, Text, View } from 'react-native';
 
 export default class CustomIntroApp extends Component {
-  buttonPressed() {
-    alert("Hello User!")
+  state = {
+    phrase: '',
+    name: '',
+  }
+  updatePhrase = (text) => {
+    this.setState({ phrase: text })
+  }
+  updateName = (text) => {
+    this.setState({ name: text })
+  }
+  intro = (phrase, name) => {
+    Alert.alert("Your Custom Introduction:", phrase + " " + name)
   }
 
   render() {
     return (
       <View style = {styles.container}>
-        <Text>Custom Introductions</Text>
+        <Text style = {styles.title}>
+          Custom Introduction
+        </Text>
+
+        <TextInput style = {styles.input}
+          underlineColorAndroid = "transparent"
+          placeholder = "Introduction Phrase"
+          placeholderTextColor = "#1B76BE"
+          autoCapitalize = "sentences"
+          onChangeText = {this.updatePhrase}
+        />
+        <TextInput style = {styles.input}
+          underlineColorAndroid = "transparent"
+          placeholder = "Your Name"
+          placeholderTextColor = "#1B76BE"
+          autoCapitalize = "sentences"
+          onChangeText = {this.updateName}
+        />
+
         <Button
-          onPress = {this.buttonPressed}
-          title = "Initiate Introduction"
+          onPress = {
+            () => this.intro(this.state.phrase, this.state.name)
+          }
+          title = "Display Introduction"
           color = "#1B76BE" 
         />
      </View>
@@ -20,11 +50,24 @@ export default class CustomIntroApp extends Component {
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    paddingTop: 50,
+  },
+  input: {
+    margin: 15,
+    height: 50,
+    padding: 5,
+    borderColor: '#1B76BE',
+    borderWidth: 2
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#1B76BE',
+    textAlign: 'center',
   },
 });
